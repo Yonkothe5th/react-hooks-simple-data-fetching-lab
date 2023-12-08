0fb2d7327279b4b7a081a4301fa9ea327460efd3
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-12-08 16:23:18 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import { useState,useEffect } from "react";
+
+
+function App(){
+    const[imgInPlace,setImgInPlace]=useState(false)
+    const[isLoaded,setisLoaded]=useState(false)
+
+    useEffect(()=> {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((resp)=>resp.json())
+        .then((data)=>{
+            setImgInPlace(data.message)
+            setisLoaded(true)
+            console.log ('render')
+        })
+    },[]);
+
+    if (!isLoaded) {
+        return <p>Loading...</p>
+    }
+
+    return <img src={imgInPlace} alt ="A Random Dog" />
+}
+
+
+export default App;
